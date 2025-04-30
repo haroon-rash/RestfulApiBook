1ed5fbe7002fcccfb59ec88583d803e9fc01bcd5
haroon <haroonurrasheed1212@gmail.com>
2025-04-30 20:23:32 +0500

    initial

---
+++ src/test/java/org/example/resfulapi/restfulapibooks/RestfulApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resfulapi.restfulapibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
